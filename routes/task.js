9041e0f6da81ddcca375bb954164346fcc6e32a2
Konrad <konrad.laczniak@gmail.com>
2019-04-26 14:37:27 +0200

    update of auth.js and task.js

--- routes/task.js
+++ routes/task.js
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
         name: req.body.name,
         done: req.body.done,
         dateStart: req.body.date,
-        tags: req.body.tagTable
+        tags: req.body.tags
     });
     task = await task.save();
 
